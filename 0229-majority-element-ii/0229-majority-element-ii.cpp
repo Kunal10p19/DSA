16e4ecc9aa4d45e6ba49e035a172145bb38a8fe3
Kunal10p19 <148734625+Kunal10p19@users.noreply.github.com>
2026-08-07 10:30:37 +0530

    Time: 7 ms (39.97%), Space: 26.6 MB (10.56%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        // Create a frequency map to store the count of each element
-        unordered_map<int, int> elementCountMap;
-        
-        // Iterate through the input array to count element occurrences
-        for(int i = 0; i < nums.size(); i++) {
-            elementCountMap[nums[i]]++;
+
+        unordered_map<int, int> mp;
+
+        // Count frequency
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
         }
-        
-        vector<int> majorityElements;
-        int threshold = nums.size() / 3;
-        
-        // Iterate through the frequency map to identify majority elements
-        for(auto elementCountPair : elementCountMap) {
-            int element = elementCountPair.first;
-            int count = elementCountPair.second;
-            
-            // Check if the element count is greater than the threshold
-            if(count > threshold) {
-                majorityElements.push_back(element);
+
+        vector<int> ans;
+
+        // Traverse the hash map
+        for (auto it : mp) {
+            if (it.second > nums.size() / 3) {
+                ans.push_back(it.first);
             }
         }
-        
-        return majorityElements; 
+
+        return ans;
     }
 };
