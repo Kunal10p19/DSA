4510b9f8f6c6183d8df939d66f83d5e3ef3f9d52
Kunal10p19 <148734625+Kunal10p19@users.noreply.github.com>
2026-08-12 12:01:09 +0530

    Time: 0 ms (100%), Space: 10.1 MB (37.73%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    vector<vector<int>> Ans;
+    int n;
+    void Helper(vector<int> &op,vector<int> &nums,int startIndex){
+
+        // base case(s)
+        if(startIndex == n){
+            Ans.push_back(op);
+            return;
+        }
+
+        // recurisve case(s)
+
+        // choice 1: you take the current element
+        op.push_back(nums[startIndex]);
+        Helper(op,nums,startIndex + 1);
+
+        // backtracking step
+        op.pop_back();
+
+        // choice 2: you neglect the current element;
+        Helper(op,nums,startIndex + 1);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums){
+        
+        vector<int> op;
+        n = nums.size();
+
+        // call the helper function
+        Helper(op,nums,0);
+
+        return Ans;
+    }
+};
